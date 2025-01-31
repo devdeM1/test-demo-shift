d25e3fb6e0aa549315a551cf3760c16ba203ba9d
devdeM1 <makd1232255@yandex.ru>
2025-01-31 17:40:31 +0300

    Add output in file

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String all_text = " ";
         try {
             BufferedReader reader = new BufferedReader(new FileReader("input_file.txt"));
             String line = reader.readLine();
             while (line != null) {
-                System.out.println(line);
+                String[] parts = line.split(",");
+                for (String element: parts){
+                    all_text = all_text.concat(element);
+                }
+                all_text = all_text.concat("\n");
                 line = reader.readLine();
             }
             reader.close();
+            System.out.println(all_text);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try(FileWriter writer = new FileWriter("output.txt", false))
+        {
+            writer.write(all_text);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 }
